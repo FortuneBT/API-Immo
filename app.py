da802b9388c088fbfd4697becf4aaa305bbcae26
becode <fortune.tshali@gmail.com>
2022-03-29 07:39:45 +0200

    change gitignore

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_restful import Api,Resource
+from typing import Optional,List,Dict
 
 
 app = Flask(__name__)
@@ -11,7 +12,37 @@ class HelloWorld(Resource):
         return {"message":"Hello World"}
 
 
-api.add_resource(HelloWorld,"/")
+#"building-state": Optional["NEW","GOOD","TO RENOVATE","JUST RENOVATED","TO REBUILD"]
+#"property-type": "APARTMENT" | "HOUSE" | "OTHERS"
+data = {
+  "data": {
+    "area": int,
+    "property-type": str,
+    "rooms-number": int,
+    "zip-code": int,
+    "land-area": Optional[int],
+    "garden": Optional[bool],
+    "garden-area": Optional[int],
+    "equipped-kitchen": Optional[bool],
+    "full-address": Optional[str],
+    "swimming-pool": Optional[bool],
+    "furnished": Optional[bool],
+    "open-fire": Optional[bool],
+    "terrace": Optional[bool],
+    "terrace-area": Optional[int],
+    "facades-number": Optional[int],
+    "building-state": Optional[str]
+  }
+}
 
+
+#output
+"""{
+  "prediction": Optional[float],
+  "error": Optional[str]
+}
+"""
+
+api.add_resource(HelloWorld,"/")
 
 app.run(debug=True)
--- test.py
+++ test.py
@@ -11,6 +11,9 @@ api = Api(app)
 class HelloWorld(Resource):
     def get(self):
         return {"message":"Hello World"}
+    
+    def post(self,response):
+        return {"message":f"Hello dear {response}"}
 
 
 api.add_resource(HelloWorld,"/")
